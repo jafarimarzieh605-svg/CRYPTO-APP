a82bd51c5e5877559933bd7d5acce3405f243819
Marzieh jafari <jafarimarzieh605@gmail.com>
2026-02-09 14:09:30 +0330

    first commit

---
+++ src/components/templates/HomePage.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import Tablecoin from "../modules/Tablecoin";
+import { getCoinList } from "../../services/cryptoApi";
+
+function HomePage() {
+    const [coins, setCoins] = useState([]);
+     
+  useEffect(() => {
+
+    const getData = async () =>{
+        const res = await fetch(getCoinList());
+        const json = await res.json();
+        setCoins(json);
+    //   fetch( getCoinList ).then((res) => res.json()).then((json) => setCoins(json));
+    };
+    getData();
+  }, []);   
+  return (
+    <div>
+        <Tablecoin coins={coins} />
+    </div>
+  )
+}
+
+export default HomePage
