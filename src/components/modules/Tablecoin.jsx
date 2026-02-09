bc80a404ec1f27ebee047ffe1044659aa2049409
Marzieh jafari <jafarimarzieh605@gmail.com>
2026-02-09 16:03:01 +0330

    The new changes is added new file ane new style.

--- src/components/modules/Tablecoin.jsx
+++ src/components/modules/Tablecoin.jsx
@@ -3,12 +3,13 @@ import { CircularProgress } from 'react-loader-spinner';
 
 import chartUp from "../../assets/chart-up.svg";
 import chartDown from "../../assets/chart-down.svg";
-
+import styles from "../modules/TableCoin.module.css"
 function Tablecoin({coins, isLoding }) {
     
   return (
-    <div>
-       {isLoding ? <CircularProgress color="#00b4d8" height="150" width="150" /> : ( <table>
+    <div className={styles.container}>
+       {isLoding ? <CircularProgress color="#00b4d8" height="150" width="150" /> : ( 
+        <table className={styles.table}>
             <thead>
                 <tr>
                     <th>Coin</th>
@@ -40,14 +41,14 @@ const TableRow = ({ coin: {name, image, symbol, total_volume, current_price, pri
         <tr>
                     
                     <td>
-                        <div>
+                        <div className={styles.symbol}>
                             <img src={image} alt="" />
                             <span>{symbol.toUpperCase()}</span>
                         </div>
                     </td>
                     <td>{name}</td> 
                      <td>${current_price.toLocaleString()}</td>
-                    <td>{price_change.toFixed(2)}%</td>
+                    <td className={price_change > 0 ? styles.success : styles.error}>{price_change.toFixed(2)}%</td>
                     <td>{total_volume.toLocaleString()}</td>
                     <td><img src={price_change > 0 ? chartUp : chartDown} alt={name} /></td>
                     
